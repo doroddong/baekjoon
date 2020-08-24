caae971dafbf212a01255f4c0977bf6d95d70e29
doroddong <52445743+doroddong@users.noreply.github.com>
2020-08-24 15:27:13 +0900

    Initial Commit

---
+++ baekjoon/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  baekjoon
+//
+//  Created by 이동훈 on 2020/08/24.
+//  Copyright © 2020 이동훈. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
